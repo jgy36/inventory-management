440725607342167c7bc9541dd429878cd5d0ae7f
jgy36 <jgyoung618@gmail.com>
2024-12-18 04:35:58 -0500

    added navbar

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import DashboardWrapper from "./dashboardWrapper";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+        <DashboardWrapper>{children}</DashboardWrapper>
+      </body>
     </html>
   );
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,3 +3,4 @@ import Dashboard from "@/app/dashboard/page";
 export default function Home() {
   return <Dashboard />;
 }
+
